1d5389a1beb4330668c812638aa10de572bd158f
marcusprice <marcusprice88@gmail.com>
2019-01-29 06:50:49 -0800

    changed k & o keys to be a higher octave

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,16 @@
 //musical typing
-let octave = 3;
+let octave = 3;//global variable to keep track of octave
 
 document.body.onkeydown = (key) => {
-  const octaveKeys = [90, 88];
+  const octaveKeys = [90, 88]; //keys to change octave down & up
   if(octaveKeys.includes(key.keyCode)) {
     switch(key.keyCode) {
       case 90:
+        //user pressed the octave down key
         octave--;
         break;
       case 88:
+        //user pressed the octave up key
         octave++;
         break;
       default:
@@ -19,6 +21,7 @@ document.body.onkeydown = (key) => {
 
   const musicalKeys = [65, 83, 68, 70, 71, 72, 74, 75, 87, 69, 84, 89, 85, 79];
   if(musicalKeys.includes(key.keyCode)) {
+    let octaveShift = 0; //shifts octave up if higher key is pressed
     let notes = {
       //major notes
       '65': 'c', //c note
@@ -28,7 +31,7 @@ document.body.onkeydown = (key) => {
       '71': 'g', //g note
       '72': 'a', //a note
       '74': 'b', //b note
-      '75': 'c', //c note
+      '75': 'c', //c note (higher octave)
       //minor notes
       '87': 'c#', //c# note
       '69': 'd#', //d# note
@@ -37,6 +40,11 @@ document.body.onkeydown = (key) => {
       '85': 'a#', //a# note
       '79': 'c#' //c# note (higher octave)
     };
-    synthesizer.triggerEnvelope(notes[key.keyCode.toString()] + octave.toString());
+    if(key.keyCode == 75 || key.keyCode == 79) {
+      //user pressed a higher octave key
+      octaveShift++;
+    }
+    //determines what note and octave to play and then triggers the synth
+    synthesizer.triggerEnvelope(notes[key.keyCode.toString()] + (octave + octaveShift).toString());
   }
 };
